19ab862d6a4fc201650e5958c32627a761549b58
Raghu Betina <raghu@firstdraft.com>
2018-07-26 02:30:02 +0000

    Whitelist columns in LandingPage for ActiveAdmin

--- app/admin/landing_pages.rb
+++ app/admin/landing_pages.rb
@@ -1,4 +1,6 @@
 ActiveAdmin.register LandingPage do
+
+ permit_params :email, :answer, :question_num
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
